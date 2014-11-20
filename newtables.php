5d6c31da38b89adb90f1f5756034112c909c769a
Surpriseshelf <surpriseshelf@gmail.com>
2014-11-20 10:23:10 +0800

    Mysql work!

--- newtables.php
+++ newtables.php
@@ -3,7 +3,7 @@
 	echo 'Trying to connect to PhoneSet!<br>';
     $mysql_server_name = "localhost"; //数据库服务器名称
     $mysql_username = "root"; // 连接数据库用户名
-    $mysql_password = "root"; // 连接数据库密码
+    $mysql_password = "20081010"; // 连接数据库密码
     $mysql_database = "TEST"; // 数据库的名字
     
 	$db = mysql_connect($mysql_server_name, $mysql_username,
